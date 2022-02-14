d54f517b20c361a59ded331b84440d3f43b2a1eb
Shota Tsutsui <k20076kk@aitech.ac.jp>
2022-02-15 00:19:54 +0900

    Fix: Cors設定の変更

--- backend/main.go
+++ backend/main.go
@@ -1,56 +1,21 @@
 package main
 
 import (
-	"context"
-	"fmt"
 	"log"
 	"net/http"
-	"os"
-	"strings"
 
-	firebase "firebase.google.com/go"
-	"github.com/gorilla/handlers"
+	"github.com/SystemEngineeringTeam/HackU-2021-vol3/apifunc"
 	"github.com/gorilla/mux"
-	"google.golang.org/api/option"
 )
 
-func VerifyCheck(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("OK\n"))
-	w.WriteHeader(http.StatusOK)
-}
-
-func MiddleWare(next_auth http.HandlerFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		//Firebase SDKの初期化
-		op := option.WithCredentialsFile(os.Getenv("CREDENTIALS"))
-		fbapp, err := firebase.NewApp(context.Background(), nil, op)
-		if err != nil {
-			fmt.Printf("Cannot initialize firebase app: %v\n", err)
-		}
-		auth, err := fbapp.Auth(context.Background())
-		if err != nil {
-			fmt.Printf("Cannot initialize firebase auth: %v\n", err)
-		}
-
-		header := r.Header.Get("Authorization") //クライアントからJWTを取得する
-		token_id := strings.Replace(header, "Bearer ", "", 1)
-
-		//JWTのベリファイ
-		token, err := auth.VerifyIDToken(context.Background(), token_id)
-		if err != nil { //認証に失敗した場合(JWTが不正な場合)は、401エラーを返す
-			fmt.Printf("Cannot verify token_id: %v\n", err)
-			w.WriteHeader(http.StatusUnauthorized)
-			return
-		}
-		log.Printf("Verified ID token: %v\n", token)
-		next_auth(w, r)
-	}
+func CORS(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "Authorization")
+	w.Header().Set("Access-Control-Allow-Methods", "GET")
 }
 
 func main() {
-	allowOrigins := handlers.AllowedOrigins([]string{"*"})
-	allowMethods := handlers.AllowedMethods([]string{"GET"})
-	allowHeaders := handlers.AllowedHeaders([]string{"Authorization"})
+
 	/*
 			func Handler(w http.ResponseWriter, r *http.Request) {
 			  w.Header().Set("Access-Control-Allow-Origin", "*")
@@ -65,6 +30,7 @@ func main() {
 	*/
 
 	router := mux.NewRouter()
-	router.HandleFunc("/auth", MiddleWare(VerifyCheck)) //http.HandleFuncと同じ役割
-	log.Fatal(http.ListenAndServe(":8080", handlers.CORS(allowOrigins, allowMethods, allowHeaders)(router)))
+	router.Handlefunc("/", CORS)
+	router.HandleFunc("/auth", apifunc.VerifyCheck) //http.HandleFuncと同じ役割
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
