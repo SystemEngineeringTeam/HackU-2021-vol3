44f85054936eb812c10e6f686023c9b1b35178ed
Shota Tsutsui <k20076kk@aitech.ac.jp>
2022-02-15 00:48:57 +0900

    Fix: Cors設定の変更

--- backend/main.go
+++ backend/main.go
@@ -5,32 +5,9 @@ import (
 	"net/http"
 
 	"github.com/SystemEngineeringTeam/HackU-2021-vol3/apifunc"
-	"github.com/gorilla/mux"
 )
 
-func CORS(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Headers", "Authorization")
-	w.Header().Set("Access-Control-Allow-Methods", "GET")
-}
-
 func main() {
-
-	/*
-			func Handler(w http.ResponseWriter, r *http.Request) {
-			  w.Header().Set("Access-Control-Allow-Origin", "*")
-			  w.Header().Set("Access-Control-Allow-Headers", "Authorization")
-			  w.Header().Set("Access-Control-Allow-Methods", "GET")
-		  }
-			のような動作と同じ
-
-			ここでオリジン、メソッド、ヘッダの許可を設定する
-			ヘッダはAuthorizationを用いた認証を行うため、これが許可されている
-			メソッドはトークンを受け取るためGETのみ
-	*/
-
-	router := mux.NewRouter()
-	router.Handlefunc("/", CORS)
-	router.HandleFunc("/auth", apifunc.VerifyCheck) //http.HandleFuncと同じ役割
+	http.HandleFunc("/auth", apifunc.VerifyCheck) //VerifyCheckをハンドラに登録
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
