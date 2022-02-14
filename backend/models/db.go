d3dccf8c2aef93fdf1f59e71d12b2765ba45cee0
SuperTikuwa <tikuwamk2@gmail.com>
2022-02-14 23:07:23 +0900

    add: db用の構造体を定義

--- backend/models/db.go
+++ backend/models/db.go
@@ -1,1 +1,39 @@
 package models
+
+type User struct {
+	ID           int    `gorm:"id"`
+	FirebaseUUID string `gorm:"firebase_uuid"`
+	Name         string `gorm:"name"`
+}
+
+type Event struct {
+	ID          int    `gorm:"id"`
+	Title       string `gorm:"title"`
+	Description string `gorm:"description"`
+	Document    string `gorm:"document"`
+	DateTime    string `gorm:"datetime"`
+	Limit       int    `gorm:"limit"`
+}
+
+type FeedBack struct {
+	EventID int    `gorm:"event_id"`
+	UserID  int    `gorm:"user_id"`
+	Stars   int    `gorm:"stars"`
+	Comment string `gorm:"comment"`
+}
+
+type Comments struct {
+	EventID int    `gorm:"event_id"`
+	UserID  int    `gorm:"user_id"`
+	Comment string `gorm:"comment"`
+}
+
+type Tags struct {
+	ID  int    `gorm:"id"`
+	Tag string `gorm:"tag"`
+}
+
+type Badges struct {
+	ID    int    `gorm:"id"`
+	Badge string `gorm:"badge"`
+}
