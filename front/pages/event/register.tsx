560ebd59e1ff4a6829f586424861accfa08daa59
hihumikan <musecharo@gmail.com>
2022-02-15 14:46:23 +0900

    svgの追加

--- front/pages/event/register.tsx
+++ front/pages/event/register.tsx
@@ -1,7 +1,16 @@
 const register = () => {
   return (
     <div>
-      <p>hello</p>
+      <p></p>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="80"
+        height="80"
+        viewBox="0 0 24 24"
+        className="fill-slate-900"
+      >
+        <path d="M12.707 17.293 8.414 13H18v-2H8.414l4.293-4.293-1.414-1.414L4.586 12l6.707 6.707z"></path>
+      </svg>
     </div>
   );
 };
