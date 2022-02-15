f71ba7f506438df42ad5257dddac4d3364954224
hihumikan <musecharo@gmail.com>
2022-02-16 01:05:20 +0900

    inputの1つ目を追加する

--- front/components/event_registration.tsx
+++ front/components/event_registration.tsx
@@ -2,7 +2,14 @@
 const event_registration = () => {
     return (
         <>
-
+            <h1>イベントの詳細</h1>
+            <button>保存</button>
+            <div className="input_1_1">
+                <label className="input_1_2">
+                    <p className="input_title">タイトル</p>
+	                <input type="text" placeholder="勉強会について説明するタイトルを描きましょう" />
+	            </label>
+            </div>
         </>
     );
 };
