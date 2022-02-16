2fd1dfb501190c8916ab6b78ae43e833901c270f
k21139 <k21139kk@aitech.ac.jp>
2022-02-16 19:58:26 +0900

    apifuncのimagesの作成

--- backend/apifunc/images.go
+++ backend/apifunc/images.go
@@ -1,1 +1,25 @@
 package apifunc
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/SystemEngineeringTeam/HackU-2021-vol3/models"
+)
+
+func ImagesGetHandler(w http.ResponseWriter, r *http.Request) {
+	b, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	var user models.Image
+	if err := json.Unmarshal(b, &user); err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(user.ID)
+	fmt.Println(user.ImageURL)
+
+}
