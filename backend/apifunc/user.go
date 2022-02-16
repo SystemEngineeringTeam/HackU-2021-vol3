084d4c2d9f04bb5fd9e0b9d8d3f131188c5fc86e
k21139 <k21139kk@aitech.ac.jp>
2022-02-17 00:14:42 +0900

    apifuncのUserPostHandlerのエラー処理

--- backend/apifunc/user.go
+++ backend/apifunc/user.go
@@ -6,6 +6,7 @@ import (
 	"io/ioutil"
 	"net/http"
 
+	"github.com/SystemEngineeringTeam/HackU-2021-vol3/dboperation"
 	"github.com/SystemEngineeringTeam/HackU-2021-vol3/models"
 )
 
@@ -41,6 +42,13 @@ func UserPostHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println(user.Name)
 	fmt.Println(user.ProfileImageURL)
 
+	err = dboperation.CreateUser(user.Name, user.ProfileImageURL, "firebaseUID")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusCreated)
+
 }
 
 func UserPutHandler(w http.ResponseWriter, r *http.Request) {
@@ -54,4 +62,17 @@ func UserPutHandler(w http.ResponseWriter, r *http.Request) {
 		fmt.Println(err)
 	}
 	fmt.Println(user.Name)
+
+	/*
+		UserPostHandlerの時に使ったdboperation.CreateUserだとprofileImageURLが入らなくて
+		何を使えば良いかわからないので代用してあります
+		とってきても使わないという状態はマズイですか
+		あと、コメント文をどこに書けば良いのかわからないのでここに書いています
+	*/
+	err = dboperation.CreateUser(user.Name, "a", "firebaseUID")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
 }
