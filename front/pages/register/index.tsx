2ad24de07d63ac3373708bae607f69976531198e
hihumikan <musecharo@gmail.com>
2022-02-17 06:33:27 +0900

    initialize

---
+++ front/pages/register/index.tsx
@@ -0,0 +1,12 @@
+import Header from "../../components/Header";
+
+const Register = () => {
+    return (
+        <div>
+            <Header />
+            <h1>Register</h1>
+        </div>
+    );
+};
+
+export default Register;
