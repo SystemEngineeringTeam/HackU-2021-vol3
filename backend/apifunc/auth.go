a774758ae0978351c5ea739e6ce51a10bbad66dd
Shota Tsutsui <k20076kk@aitech.ac.jp>
2022-02-16 14:06:29 +0900

    Fix: envファイルの呼び出し方の変更，ctxの処理の追加

--- backend/apifunc/auth.go
+++ backend/apifunc/auth.go
@@ -20,39 +20,45 @@ func VerifyCheck(w http.ResponseWriter, r *http.Request) {
 	//Access-ControlをVerifyCheck内にも適用
 
 	//Firebase SDKの初期化
-	err := godotenv.Load("../env/credentials.env")
+	err := godotenv.Load("firebase/.env")
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
-	credentials := os.Getenv("CREDENTIALS")
+
 	//envファイルからcredentials(秘密鍵)を取得
 	/*CREDENTIALSはfirebaseの設定で作成したjsonファイルのパス,以下のような感じでcredentials.envにCREDENTIALSを記述
 	CREDENTIALS=/Users/<ユーザ名>/firebase/<Firebase SDKの秘密鍵の名前>.json
 	*/
+	ctx := r.Context()
+	if ctx == nil {
+		ctx = context.Background()
+	}
 
-	context := context.Background()
-
-	op := option.WithCredentialsFile(os.Getenv(credentials))
-	fbapp, err := firebase.NewApp(context, nil, op)
+	opt := option.WithCredentialsFile(os.Getenv("CREDENTIALS"))
+	app, err := firebase.NewApp(ctx, nil, opt)
 	if err != nil {
 		fmt.Printf("Cannot initialize firebase app: %v\n", err)
 	}
-	auth, err := fbapp.Auth(context)
+	auth, err := app.Auth(ctx)
 	if err != nil {
 		fmt.Printf("Cannot initialize firebase auth: %v\n", err)
 	}
 
 	header := r.Header.Get("Authorization") //クライアントからJWTを取得する
 	token_id := strings.Replace(header, "Bearer ", "", 1)
-
+	fmt.Println(token_id)
 	//JWTのベリファイ
-	token, err := auth.VerifyIDToken(context, token_id)
+	token, err := auth.VerifyIDToken(ctx, token_id)
 	if err != nil { //認証に失敗した場合(JWTが不正な場合)は、401エラーを返す
 		fmt.Printf("Cannot verify token_id: %v\n", err)
 		w.WriteHeader(http.StatusUnauthorized)
 		return
 	}
 	log.Printf("Verified ID token: %v\n", token)
+
+	uid := token.Claims["user_id"]
+	log.Printf("Verified user_id: %v\n", uid)
+
 	w.Write([]byte("OK\n"))
 	w.WriteHeader(http.StatusOK)
 }
