b49aae8a997ecc594257f52edd1218cc1b0aa9c2
SuperTikuwa <tikuwamk2@gmail.com>
2022-02-20 00:40:47 +0900

    add: ユーザが参加登録しているイベントを取得

--- backend/dboperation/register_test.go
+++ backend/dboperation/register_test.go
@@ -1,6 +1,9 @@
 package dboperation
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestJoinEvent(t *testing.T) {
 	err := JoinEvent(1, 1)
@@ -13,5 +16,16 @@ func TestLeaveEvent(t *testing.T) {
 	err := LeaveEvent(1, 1)
 	if err != nil {
 		t.Error(err)
+	}
+}
+
+func TestSelectRegisteredEvents(t *testing.T) {
+	e, err := SelectRegisteredEvents(4)
+	if err != nil {
+		t.Error(err)
+	}
+
+	for _, v := range e {
+		fmt.Println(v.Title)
 	}
 }
