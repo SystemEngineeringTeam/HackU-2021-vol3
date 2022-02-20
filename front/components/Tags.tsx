2d1e3faffd72048247af2c81d74a402cb3a53bc2
hihumikan <musecharo@gmail.com>
2022-02-20 17:20:29 +0900

    タグのレイアウトの暫定

---
+++ front/components/Tags.tsx
@@ -0,0 +1,367 @@
+import { Menu, Transition } from '@headlessui/react'
+import { ChevronDownIcon } from '@heroicons/react/solid'
+import { Fragment, useEffect, useRef, useState } from 'react'
+
+export default function Tags() {
+    return (
+        <div className="text-left">
+            <Menu as="div" className="inline-block relative text-left">
+                <div>
+                    <Menu.Button className="inline-flex justify-center py-2 px-4 pt-4 w-full text-sm font-medium text-gray-800 bg-white rounded-md border-2 border-slate-500 border-opacity-70 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-100">
+                        タグの追加
+
+                        <svg xmlns="http://www.w3.org/2000/svg" className="-mt-2 ml-2 w-8 h-8 tag_svg" viewBox="0 0 20 20" fill="currentColor">
+                            <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-11a1 1 0 10-2 0v2H7a1 1 0 100 2h2v2a1 1 0 102 0v-2h2a1 1 0 100-2h-2V7z" clipRule="evenodd" />
+                        </svg>
+
+                    </Menu.Button>
+                </div>
+                <Transition
+                    as={Fragment}
+                    enter="transition ease-out duration-100"
+                    enterFrom="transform opacity-0 scale-95"
+                    enterTo="transform opacity-100 scale-100"
+                    leave="transition ease-in duration-75"
+                    leaveFrom="transform opacity-100 scale-100"
+                    leaveTo="transform opacity-0 scale-95"
+                >
+                    <Menu.Items className="absolute right-0 mt-2 w-56 bg-white rounded-md divide-y divide-gray-100 focus:outline-none ring-1 ring-black ring-opacity-5 shadow-lg origin-top-right">
+                        <div className="p-1">
+                            <Menu.Item>
+                                {({ active }) => (
+                                    <button
+                                        className={`${active ? 'bg-slate-700 text-white' : 'text-gray-900'
+                                            } group flex rounded-md items-center w-full px-2 py-2 text-sm`}
+                                    >
+                                        {active ? (
+                                            <EditActiveIcon
+                                                className="mr-2 w-5 h-5"
+                                                aria-hidden="true"
+                                            />
+                                        ) : (
+                                            <EditInactiveIcon
+                                                className="mr-2 w-5 h-5"
+                                                aria-hidden="true"
+                                            />
+                                        )}
+                                        Edit
+                                    </button>
+                                )}
+                            </Menu.Item>
+                            <Menu.Item>
+                                {({ active }) => (
+                                    <button
+                                        className={`${active ? 'bg-slate-700 text-white' : 'text-gray-900'
+                                            } group flex rounded-md items-center w-full px-2 py-2 text-sm`}
+                                    >
+                                        {active ? (
+                                            <DuplicateActiveIcon
+                                                className="mr-2 w-5 h-5"
+                                                aria-hidden="true"
+                                            />
+                                        ) : (
+                                            <DuplicateInactiveIcon
+                                                className="mr-2 w-5 h-5"
+                                                aria-hidden="true"
+                                            />
+                                        )}
+                                        Duplicate
+                                    </button>
+                                )}
+                            </Menu.Item>
+                        </div>
+                        <div className="p-1">
+                            <Menu.Item>
+                                {({ active }) => (
+                                    <button
+                                        className={`${active ? 'bg-slate-700 text-white' : 'text-gray-900'
+                                            } group flex rounded-md items-center w-full px-2 py-2 text-sm`}
+                                    >
+                                        {active ? (
+                                            <ArchiveActiveIcon
+                                                className="mr-2 w-5 h-5"
+                                                aria-hidden="true"
+                                            />
+                                        ) : (
+                                            <ArchiveInactiveIcon
+                                                className="mr-2 w-5 h-5"
+                                                aria-hidden="true"
+                                            />
+                                        )}
+                                        Archive
+                                    </button>
+                                )}
+                            </Menu.Item>
+                            <Menu.Item>
+                                {({ active }) => (
+                                    <button
+                                        className={`${active ? 'bg-slate-700 text-white' : 'text-gray-900'
+                                            } group flex rounded-md items-center w-full px-2 py-2 text-sm`}
+                                    >
+                                        {active ? (
+                                            <MoveActiveIcon
+                                                className="mr-2 w-5 h-5"
+                                                aria-hidden="true"
+                                            />
+                                        ) : (
+                                            <MoveInactiveIcon
+                                                className="mr-2 w-5 h-5"
+                                                aria-hidden="true"
+                                            />
+                                        )}
+                                        Move
+                                    </button>
+                                )}
+                            </Menu.Item>
+                        </div>
+                        <div className="p-1">
+                            <Menu.Item>
+                                {({ active }) => (
+                                    <button
+                                        className={`${active ? 'bg-slate-700 text-white' : 'text-gray-900'
+                                            } group flex rounded-md items-center w-full px-2 py-2 text-sm`}
+                                    >
+                                        {active ? (
+                                            <DeleteActiveIcon
+                                                className="mr-2 w-5 h-5 text-violet-400"
+                                                aria-hidden="true"
+                                            />
+                                        ) : (
+                                            <DeleteInactiveIcon
+                                                className="mr-2 w-5 h-5 text-violet-400"
+                                                aria-hidden="true"
+                                            />
+                                        )}
+                                        Delete
+                                    </button>
+                                )}
+                            </Menu.Item>
+                        </div>
+                    </Menu.Items>
+                </Transition>
+            </Menu>
+        </div>
+    )
+}
+
+function EditInactiveIcon(props: any) {
+    return (
+        <svg
+            {...props}
+            viewBox="0 0 20 20"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <path
+                d="M4 13V16H7L16 7L13 4L4 13Z"
+                fill="#EDE9FE"
+                stroke="#A78BFA"
+                strokeWidth="2"
+            />
+        </svg>
+    )
+}
+
+function EditActiveIcon(props: any) {
+    return (
+        <svg
+            {...props}
+            viewBox="0 0 20 20"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <path
+                d="M4 13V16H7L16 7L13 4L4 13Z"
+                fill="#8B5CF6"
+                stroke="#C4B5FD"
+                strokeWidth="2"
+            />
+        </svg>
+    )
+}
+
+function DuplicateInactiveIcon(props: any) {
+    return (
+        <svg
+            {...props}
+            viewBox="0 0 20 20"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <path
+                d="M4 4H12V12H4V4Z"
+                fill="#EDE9FE"
+                stroke="#A78BFA"
+                strokeWidth="2"
+            />
+            <path
+                d="M8 8H16V16H8V8Z"
+                fill="#EDE9FE"
+                stroke="#A78BFA"
+                strokeWidth="2"
+            />
+        </svg>
+    )
+}
+
+function DuplicateActiveIcon(props: any) {
+    return (
+        <svg
+            {...props}
+            viewBox="0 0 20 20"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <path
+                d="M4 4H12V12H4V4Z"
+                fill="#8B5CF6"
+                stroke="#C4B5FD"
+                strokeWidth="2"
+            />
+            <path
+                d="M8 8H16V16H8V8Z"
+                fill="#8B5CF6"
+                stroke="#C4B5FD"
+                strokeWidth="2"
+            />
+        </svg>
+    )
+}
+
+function ArchiveInactiveIcon(props: any) {
+    return (
+        <svg
+            {...props}
+            viewBox="0 0 20 20"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <rect
+                x="5"
+                y="8"
+                width="10"
+                height="8"
+                fill="#EDE9FE"
+                stroke="#A78BFA"
+                strokeWidth="2"
+            />
+            <rect
+                x="4"
+                y="4"
+                width="12"
+                height="4"
+                fill="#EDE9FE"
+                stroke="#A78BFA"
+                strokeWidth="2"
+            />
+            <path d="M8 12H12" stroke="#A78BFA" strokeWidth="2" />
+        </svg>
+    )
+}
+
+function ArchiveActiveIcon(props: any) {
+    return (
+        <svg
+            {...props}
+            viewBox="0 0 20 20"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <rect
+                x="5"
+                y="8"
+                width="10"
+                height="8"
+                fill="#8B5CF6"
+                stroke="#C4B5FD"
+                strokeWidth="2"
+            />
+            <rect
+                x="4"
+                y="4"
+                width="12"
+                height="4"
+                fill="#8B5CF6"
+                stroke="#C4B5FD"
+                strokeWidth="2"
+            />
+            <path d="M8 12H12" stroke="#A78BFA" strokeWidth="2" />
+        </svg>
+    )
+}
+
+function MoveInactiveIcon(props: any) {
+    return (
+        <svg
+            {...props}
+            viewBox="0 0 20 20"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <path d="M10 4H16V10" stroke="#A78BFA" strokeWidth="2" />
+            <path d="M16 4L8 12" stroke="#A78BFA" strokeWidth="2" />
+            <path d="M8 6H4V16H14V12" stroke="#A78BFA" strokeWidth="2" />
+        </svg>
+    )
+}
+
+function MoveActiveIcon(props: any) {
+    return (
+        <svg
+            {...props}
+            viewBox="0 0 20 20"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <path d="M10 4H16V10" stroke="#C4B5FD" strokeWidth="2" />
+            <path d="M16 4L8 12" stroke="#C4B5FD" strokeWidth="2" />
+            <path d="M8 6H4V16H14V12" stroke="#C4B5FD" strokeWidth="2" />
+        </svg>
+    )
+}
+
+function DeleteInactiveIcon(props: any) {
+    return (
+        <svg
+            {...props}
+            viewBox="0 0 20 20"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <rect
+                x="5"
+                y="6"
+                width="10"
+                height="10"
+                fill="#EDE9FE"
+                stroke="#A78BFA"
+                strokeWidth="2"
+            />
+            <path d="M3 6H17" stroke="#A78BFA" strokeWidth="2" />
+            <path d="M8 6V4H12V6" stroke="#A78BFA" strokeWidth="2" />
+        </svg>
+    )
+}
+
+function DeleteActiveIcon(props: any) {
+    return (
+        <svg
+            {...props}
+            viewBox="0 0 20 20"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <rect
+                x="5"
+                y="6"
+                width="10"
+                height="10"
+                fill="#8B5CF6"
+                stroke="#C4B5FD"
+                strokeWidth="2"
+            />
+            <path d="M3 6H17" stroke="#C4B5FD" strokeWidth="2" />
+            <path d="M8 6V4H12V6" stroke="#C4B5FD" strokeWidth="2" />
+        </svg>
+    )
+}
