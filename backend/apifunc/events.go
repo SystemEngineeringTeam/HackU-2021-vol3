d0736f8969af4b56470938583be1d096926d2f12
Shota Tsutsui <k20076kk@aitech.ac.jp>
2022-02-20 15:33:12 +0900

    Add:EventIdGetResponseでIDからイベント取得，EventGetHandlerでイベント全取得

--- backend/apifunc/events.go
+++ backend/apifunc/events.go
@@ -12,7 +12,34 @@ import (
 	"github.com/gorilla/mux"
 )
 
-func EventGetHandler(w http.ResponseWriter, r *http.Request){
+func EventGetHandler(w http.ResponseWriter, r *http.Request) {
+	events, err := dboperation.SelectEvents()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	response := make([]models.EventGetResponse, 0)
+	for _, event := range events {
+		e := models.EventGetResponse{
+			ID:        event.ID,
+			Title:     event.Title,
+			ImageURL:  event.ImageURL,
+			Organizer: event.Organizer,
+			DateTime:  event.DateTime,
+			Tags:      event.Tags,
+		}
+		response = append(response, e)
+	}
+
+	j, err := json.Marshal(response)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Fprint(w, string(j))
+}
+
+func EventIdGetHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	id, _ := strconv.Atoi(vars["id"])
 	//get pathparameter
@@ -70,25 +97,25 @@ func EventPostHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusCreated)
 }
 
-func StreamURLPostHandler(w http.ResponseWriter, r *http.Request){
-	vars := mux.Vars(r)
-	id, _ := strconv.Atoi(vars["id"])
+func StreamURLPostHandler(w http.ResponseWriter, r *http.Request) {
+	// vars := mux.Vars(r)
+	// id, _ := strconv.Atoi(vars["id"])
 
-	event, err := dboperation.SelectEventByID(id)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-	streamURL := event.StreamURL
-	fmt.Println(streamURL)
-
-	// err = dboperation.UpdateStreamURL(streamURL, id)
+	// event, err := dboperation.SelectEventByID(id)
 	// if err != nil {
 	// 	http.Error(w, err.Error(), http.StatusBadRequest)
 	// 	return
 	// }
+	// streamURL := event.StreamURL
+	// fmt.Println(streamURL)
 
-	w.WriteHeader(http.StatusOK)
+	// err = dboperation.UpdateStreamURL(streamURL, id)
+	// if err != nil {
+	// 	http.Error(w, err.Error(), http.StatusBadRequest)
+	//  return
+	// }
+
+	// w.WriteHeader(http.StatusOK)
 }
 
 func EventPutHandler(w http.ResponseWriter, r *http.Request) {
@@ -115,128 +142,47 @@ func EventPutHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	w.WriteHeader(http.StatusOK)
 }
-// func FeedbackPostHandler(w http.ResponseWriter, r *http.Request) {
-// 	/*
+func FeedbackPostHandler(w http.ResponseWriter, r *http.Request) {
+	/*
 
-// 		WIP
+		WIP
 
-// 	*/
+	*/
 
-// 	b, err := ioutil.ReadAll(r.Body)
-// 	if err != nil {
-// 		fmt.Println(err)
-// 	}
-// 	var feedback models.FeedbackPostRequest
-// 	var user models.UserIdGetResponse
+	b, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+	var feedback models.FeedbackPostRequest
+	var user models.UserIdGetResponse
 
-// 	if err := json.Unmarshal(b, &feedback); err != nil {
-// 		fmt.Println(err)
-// 	}
-// 	if err := json.Unmarshal(b, &user); err != nil {
-// 		fmt.Println(err)
-// 	} //UserIDをどうやって取得するか？
+	if err := json.Unmarshal(b, &feedback); err != nil {
+		fmt.Println(err)
+	}
+	if err := json.Unmarshal(b, &user); err != nil {
+		fmt.Println(err)
+	} //UserIDをどうやって取得するか？
 
-// 	fmt.Println(user.ID)
-// 	fmt.Println(feedback.Comment)
-// 	fmt.Println(feedback.Stars)
+	fmt.Println(user.ID)
+	fmt.Println(feedback.Comment)
+	fmt.Println(feedback.Stars)
 
-// 	vars := mux.Vars(r)
-// 	eventID, _ := strconv.Atoi(vars["id"])
+	vars := mux.Vars(r)
+	eventID, _ := strconv.Atoi(vars["id"])
 
-// 	fmt.Println(vars, eventID)
+	fmt.Println(vars, eventID)
 
-// 	// e := models.FeedBack{
-// 	// 	EventID: eventID,
-// 	// 	UserID:  user.Id,
-// 	// 	Stars:   feedback.Stars,
-// 	// 	Comment: feedback.Comment,
-// 	// }
+	// e := models.FeedBack{
+	// 	EventID: eventID,
+	// 	UserID:  user.Id,
+	// 	Stars:   feedback.Stars,
+	// 	Comment: feedback.Comment,
+	// }
 
-// 	// err = dboperation.CreateFeedback(e)
-// 	// if err != nil {
-// 	// 	http.Error(w, err.Error(), http.StatusInternalServerError)
-// 	// 	return
-// 	// }
-// 	w.WriteHeader(http.StatusCreated)
-// }
-
-// func CommentGetHandler(w http.ResponseWriter, r *http.Request) {
-// 	/*
-
-// 		WIP
-
-// 	*/
-
-// 	// comments, err := dboperation.GetAllComments()
-// 	// if err != nil {
-// 	// 	http.Error(w, err.Error(), http.StatusInternalServerError)
-// 	// 	return
-// 	// } //どうやってコメントを取得するか？
-
-// 	// vars := mux.Vars(r)
-// 	// eventID, _ := strconv.Atoi(vars["id"])
-
-// 	// fmt.Println(vars, eventID)
-
-// 	// response := make([]models.CommentGetAndPostRequest, 0)
-
-// 	// for _, comm := range comments {
-// 	// 	if eventID == comm.EventID {
-// 	// 		r := models.CommentGetAndPostRequest{
-// 	// 			Comment: comm,
-// 	// 		}
-// 	// 		response = append(response, r)
-// 	// 	}
-// 	// }
-
-// 	// b, err := json.Marshal(response)
-// 	// if err != nil {
-// 	// 	fmt.Println(err)
-// 	// }
-
-// 	// fmt.Fprint(w, string(b))
-// }
-
-// func CommentPostHandler(w http.ResponseWriter, r *http.Request) {
-// 	/*
-
-// 		WIP
-
-// 	*/
-// 	b, err := ioutil.ReadAll(r.Body)
-// 	if err != nil {
-// 		fmt.Println(err)
-// 	}
-
-// 	var comm models.CommentGetAndPostRequest
-// 	var user models.UserIdGetResponse
-
-// 	if err := json.Unmarshal(b, &comm); err != nil {
-// 		fmt.Println(err)
-// 	}
-
-// 	if err := json.Unmarshal(b, &user); err != nil {
-// 		fmt.Println(err)
-// 	}
-// 	fmt.Println(comm.Comment)
-// 	fmt.Println(user.ID)
-// 	//Userが取得できているかチェック
-// 	vars := mux.Vars(r)
-// 	eventID, _ := strconv.Atoi(vars["id"])
-
-// 	fmt.Println(vars, eventID)
-
-// 	// e := models.Comments{
-// 	// 	EventID: eventID,
-// 	// 	UserID:  user.Id,
-// 	// 	Comment: comm.Comment,
-// 	// }
-
-// 	// err = dboperation.PostComment(e)
-// 	// if err != nil {
-// 	// 	http.Error(w, err.Error(), http.StatusBadRequest)
-// 	// 	return
-// 	// }
-
-// 	w.WriteHeader(http.StatusCreated)
-// }
+	// err = dboperation.CreateFeedback(e)
+	// if err != nil {
+	// 	http.Error(w, err.Error(), http.StatusInternalServerError)
+	// 	return
+	// }
+	w.WriteHeader(http.StatusCreated)
+}
